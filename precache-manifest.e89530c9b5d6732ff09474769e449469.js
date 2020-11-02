443838af3ba5f4057658adfaaa4bcab45c29c10f
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-11-02 22:11:05 +0100

    Updates

---
+++ precache-manifest.e89530c9b5d6732ff09474769e449469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c49e46cc4de00fda76b40855494f18",
+    "url": "/SocialNetwork-JS/index.html"
+  },
+  {
+    "revision": "c829b3a6e257d8211e70",
+    "url": "/SocialNetwork-JS/static/css/main.98178dcc.chunk.css"
+  },
+  {
+    "revision": "d3c579e7d6ea48386710",
+    "url": "/SocialNetwork-JS/static/js/2.4768857f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork-JS/static/js/2.4768857f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c829b3a6e257d8211e70",
+    "url": "/SocialNetwork-JS/static/js/main.2bd42f3c.chunk.js"
+  },
+  {
+    "revision": "20054e54ca1e57b9ca4f",
+    "url": "/SocialNetwork-JS/static/js/runtime-main.8fe00e27.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SocialNetwork-JS/precache-manifest.69c699cda017018bba9a3362789ebfbb.js"
+  "/SocialNetwork-JS/precache-manifest.e89530c9b5d6732ff09474769e449469.js"
 );
 
 self.addEventListener('message', (event) => {
