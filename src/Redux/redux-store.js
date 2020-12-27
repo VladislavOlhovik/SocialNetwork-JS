3d9b95ee0341b5a0c8644f9f7a4d96f09c5aa751
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-12-27 19:45:45 +0100

    redux devtools 91

--- src/Redux/redux-store.js
+++ src/Redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import authReducer from "./auth-reducer";
 import dialogsReducer from "./dialogs-reducer";
@@ -18,6 +18,9 @@ let reducers = combineReducers({
     app: appReducer,
 });
 
-let store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+
+// let store = createStore(reducers, applyMiddleware(thunk));
 window.store = store
 export default store;
